5a195e4b7c0797ec7e62b41950d9559edcc46756
husterknupp <4.23@gmx.de>
2014-05-02 11:28:34 +0200

    initial commit

---
+++ src/main/java/de/bschandera/jeetutorials/config/Config.java
@@ -0,0 +1,26 @@
+package de.bschandera.jeetutorials.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+// Marks this class as configuration
+// Specifies which package to scan
+@ComponentScan("de.bschandera.jeetutorials.config")
+// Enables Spring's annotations
+@EnableWebMvc
+public class Config extends WebMvcConfigurerAdapter {
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+	UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+	resolver.setPrefix("/WEB-INF/views/");
+	resolver.setSuffix(".jsp");
+	resolver.setViewClass(JstlView.class);
+	return resolver;
+    }
+}
